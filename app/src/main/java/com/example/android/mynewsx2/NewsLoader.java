0c8336dee7383c9364e0010b4143654f6a852810
ahmed-abdelazim <34234821+ahmed-abdelazim@users.noreply.github.com>
2018-08-12 12:28:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/mynewsx2/NewsLoader.java
@@ -0,0 +1,43 @@
+package com.example.android.mynewsx2;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Created by Person on 11/08/2018.
+ */
+
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+
+
+    public NewsLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+    @Override
+    public List<News> loadInBackground() {
+        List<News> listOfNews = null;
+        try {
+            URL url = QueryUtils.createUrl();
+            String jsonResponse = QueryUtils.makeHttpRequest(url);
+            listOfNews = QueryUtils.parseJson(jsonResponse);
+        } catch (IOException e) {
+            Log.e("Queryutils", "Error Loader LoadInBackground: ", e);
+        }
+        return listOfNews;
+    }
+
+}
